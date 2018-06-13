f518203625cd51767eb2f8dceaaa2dbf2de64789
Yugeun Shim <cozyyg@naver.com>
2018-06-13 10:57:21 +0900

    Update irc.py

--- irc.py
+++ irc.py
@@ -13,23 +13,24 @@ class IRC:
     def send(self, chan, msg):
         self.irc.send(bytes("PRIVMSG " + chan + " :" + msg + "\n","utf-8"))
  
-    def connect(self, server, channel, botnick):
+    def connect(self, server, botnick):
         #defines the socket
         print ("connecting to:"+server)
         self.irc.connect((server, 16664))  #connects to the server
         self.irc.send(bytes("PASS PASS\n","utf-8"))
         self.irc.send(bytes("USER " + botnick + " " + "0" +" " + "*" + " :Dolchang-bot\n","utf-8")) #user authentication
         self.irc.send(bytes("NICK " + botnick + "\n","utf-8"))
-        self.irc.send(bytes("JOIN " + channel + "\n","utf-8"))        #join the chan
         
     def quit(self, message):
         self.irc.send("QUIT " + message)
+        
+    def join(self, chan):
+        self.irc.send(bytes("JOIN " + chan + "\n", "utf-8"))
  
     def get_text(self):
-        try:
-            text=self.irc.recv(2048).decode("utf-8")  #receive the text
-        except:
-            text = ""
+        text=self.irc.recv(2048)  #receive the text
+        if text.strip():
+            text=text.decode("utf-8")
  
         if text.find('PING') != -1:                      
             self.irc.send(bytes('PONG ' + text.split() [1] + '\n',"utf-8"))
@@ -39,6 +40,3 @@ class IRC:
         
     def opping(self, chan, name):
         self.irc.send(bytes("MODE " + chan + " +o " + name + "\n","utf-8"))
-        
-    def deopping(self, chan, name):
-        self.irc.send(bytes("MODE " + chan + " -o " + name + "\n","utf-8"))
